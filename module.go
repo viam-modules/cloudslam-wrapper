f0a85af7a461a8f0849eb25bb2a16b2d7a185575
John <john@viam.com>
2024-07-16 16:12:40 -0400

    initial module without code

---
+++ module.go
@@ -0,0 +1,37 @@
+// package main implements a slam service that wraps cloudslam
+package main
+
+import (
+	"cloudslam-module/cloudslam"
+	"context"
+
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/module"
+	"go.viam.com/rdk/services/slam"
+	"go.viam.com/utils"
+)
+
+func main() {
+	utils.ContextualMain(mainWithArgs, module.NewLoggerFromArgs("cloudslam_wrapper"))
+}
+
+func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) (err error) {
+	customModule, err := module.NewModuleFromArgs(ctx, logger)
+	if err != nil {
+		return err
+	}
+
+	err = customModule.AddModelFromRegistry(ctx, slam.API, cloudslam.Model)
+	if err != nil {
+		return err
+	}
+
+	err = customModule.Start(ctx)
+	defer customModule.Close(ctx)
+	if err != nil {
+		return err
+	}
+
+	<-ctx.Done()
+	return nil
+}
